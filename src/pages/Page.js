98bcbd2fdd7529fdc15e980b6f00e008f9b2d992
A.Sch <a.sch@skbkontur.ru>
2022-06-01 17:57:21 +0500

    toasts

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tooltip} from 'bootstrap/dist/js/bootstrap.esm.min.js';
+import {Tooltip, Toast} from 'bootstrap/dist/js/bootstrap.esm.min.js';
 
 
 const { useState, useEffect, useRef, useLayoutEffect } = React;
@@ -25,14 +25,98 @@ function Page() {
     document.title = `${count}`;
   });
 
+  var [toast, setToast] = useState(false);
+  const toastRef = useRef();
+
+  useEffect(() => {
+      var myToast = toastRef.current;
+      var bsToast = Toast.getInstance(myToast);
+
+      if (!bsToast) {
+          bsToast = new Toast(myToast, {autohide: true});
+          bsToast.hide();
+          setToast(false);
+          myToast.addEventListener('hidden.bs.toast', function () {
+            setToast(false)
+          });
+      }
+      else {
+          toast ? bsToast.show() : bsToast.hide();
+
+      }
+  })
+
+
+  var [toasts, setToasts] = useState([]);
+
+  useEffect(() => {
+    let toast = document.querySelector(`#qwe${toasts.length - 1}`);
+    if (toast) {
+      let a = new Toast(toast);
+      a.show();
+
+      toast.addEventListener('hidden.bs.toast', function () {
+        toast.remove()
+      });
+    }
+  })
+
+  function pushToast() {
+    let toastArray = [...toasts];
+    let toast = (
+      <div className="toast m-3" id={'qwe' + toastArray.length} key={toastArray.length}>
+        <div className="toast-header">
+          <strong className="me-auto">Bootstrap</strong>
+          <small className="text-muted">{(new Date()).toLocaleTimeString()}</small>
+          <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div className="toast-body text-start">
+          See? Just like this. {toastArray.length}
+        </div>
+      </div>
+    )
+
+    toastArray.push(toast);
+    setToasts(toastArray);
+  }
+
   return (
     <div>
       {count}
+      <div className="toast-container top-0 end-0 position-absolute">
+        {toasts}
+      </div>
+      <button className="btn btn-info" onClick={pushToast}>
+          Push toast
+      </button>
+
       <div>
         <button className="btn btn-info" onClick={handleClick} ref={tooltipRef}>
             Hover for tooltip
         </button>
       </div>
+
+      <button className="btn btn-success" onClick={() => setToast(toast => !toast)}>
+           Toast {toast?'hide':'show'}
+       </button>
+
+      <div className="toast top-0 end-0 position-absolute m-3" role="alert" aria-live="assertive" aria-atomic="true" ref={toastRef}>
+        <div className="toast-header">
+          <strong className="me-auto">Bootstrap</strong>
+          <small className="text-muted">just now</small>
+          <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div className="toast-body">
+          See? Just like this.
+        </div>
+      </div>
+
+       <div className="toast position-absolute m-4" role="alert" >
+           <div className="toast-body">
+             Hello, world! This is a toast message.
+           </div>
+       </div>
+
     </div>
   );
 }
