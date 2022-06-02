99bcdab176661a1385cc6331be6985572701d797
A.Sch <a.sch@skbkontur.ru>
2022-06-03 02:46:09 +0500

    navbar, bs icons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import React from 'react';
 
 import IndexLayout from './layouts/IndexLayout.js';
--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -13,12 +13,23 @@ export default function Navbar() {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+            <a className="nav-link active" aria-current="page" href="/">
+              <i class="bi bi-bookmark-fill me-2"></i>
+              Home
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/orders">Todos</a>
+            <a className="nav-link" aria-current="page" href="/orders">Todos</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/orders">Todos</a>
           </li>
         </ul>
+        <div>
+          <a className="btn btn-primary py-1" href="#">
+            <i class="bi bi-person-fill"></i> Вход
+          </a>
+        </div>
       </div>
     </div>
   </nav>
